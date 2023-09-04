a5ba59a432f6be7868c6c72d4acb46dc80fad77c
Pietro Castelli <pietrocast03@gmail.com>
2023-09-04 20:52:07 +0200

    center title

--- src/components/FilterShow.tsx
+++ src/components/FilterShow.tsx
@@ -1,42 +1,46 @@
-import { Input } from "./Input";
-import { ComboBox } from "./ComboBox";
-import { Select } from "./Select";
-import { Button } from "./Button";
-export function FilterShow() {
+import { Input } from './Input'
+import { ComboBox } from './ComboBox'
+import { Select } from './Select'
+import { Button } from './Button'
+export function FilterShow () {
   return (
     <>
-      <div className="filtershow">
-        <span>top 100 anime</span>
-        <div className="filtershow-container">
-          <div className="filtershow-items">
-            <div className="filtershow-element">
-              <span>search</span>
-              <Input />
-            </div>
-            <div className="filtershow-element">
-              <span>genres</span>
-              <ComboBox />
-            </div>
-            <div className="filtershow-element">
-              <span>year</span>
-              <ComboBox />
-            </div>
-            <div className="filtershow-element">
-              <span>season</span>
-              <Select />
-            </div>
-            <div className="filtershow-element">
-              <span>format</span>
-              <Select />
-            </div>
-            <div className="filtershow-element">
-              <span>airing status</span>
-              <Select />
-            </div>
+      <div className='filtershow'>
+        <div>
+          <div>
+            <span>top 100 anime</span>
           </div>
-          <Button />
+          <div className='filtershow-container'>
+            <div className='filtershow-items'>
+              <div className='filtershow-element'>
+                <span>search</span>
+                <Input />
+              </div>
+              <div className='filtershow-element'>
+                <span>genres</span>
+                <ComboBox />
+              </div>
+              <div className='filtershow-element'>
+                <span>year</span>
+                <ComboBox />
+              </div>
+              <div className='filtershow-element'>
+                <span>season</span>
+                <Select />
+              </div>
+              <div className='filtershow-element'>
+                <span>format</span>
+                <Select />
+              </div>
+              <div className='filtershow-element'>
+                <span>airing status</span>
+                <Select />
+              </div>
+            </div>
+            <Button />
+          </div>
         </div>
       </div>
     </>
-  );
+  )
 }
