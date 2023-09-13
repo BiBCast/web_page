d302bff950347e72865374eaff0a9d9fae6b25bc
BiBCast <pietrocast03@gmail.com>
2023-09-13 09:26:16 +0200

    add duoble input bar

--- src/components/SearchOptionsButton.tsx
+++ src/components/SearchOptionsButton.tsx
@@ -1,18 +1,20 @@
-import { Select } from './Select'
-export function SearchOptionsButton () {
+import MultiRangeSlider from "./MultiRangeSlider/MultiRangeSlider";
+import { Select } from "./Select";
+export function SearchOptionsButton() {
   return (
     <>
-      <div className='opt-menu'>
-        <div className='state-show'>
-          <Select />
+      <div className="opt-menu">
+        <div className="state-show">
           <Select />
           <Select />
           <Select />
         </div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className="select-show">
+          <MultiRangeSlider min={0} max={100} />
+          <MultiRangeSlider min={0} max={100} />
+          <MultiRangeSlider min={0} max={100} />
+        </div>
       </div>
     </>
-  )
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import './css/filtershow.css'
-import './css/navbar.css'
-import './css/tooltip.css'
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "./css/filtershow.css";
+import "./css/navbar.css";
+import "./css/tooltip.css";
+import "./css/searchOptionsButton.css";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
